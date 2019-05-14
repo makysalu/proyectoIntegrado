fbda365f3b0bc552a37d499c1ffd209b2669c978
makysalu <44227511+makysalu@users.noreply.github.com>
2019-05-14 14:10:59 +0200

    Introducimos parametros de la pagina deletuser.php

---
+++ proyectoupy/public/deletuser.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+    if (!isset($_SESSION["admin"])) header("Location: http://localhost/web/programacion/evaluacion3/proyectoupy/public/login_admin.php");
+    require "./../src/BBDD.php";
+    require "./../src/Administrador.php";
+    $a = new Administrador();
+    $a->conexion();
+    $resultado=$a->usuarios();
+    ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Eliminar Usuario</title>
+    <link rel="stylesheet" href="css/stilesadmin.css">
+  </head>
+  <body>
+    	<?php include "./assets/navegadoradmin.php"; ?>
+      <section class="eliminar_usuario">
+        <h1>Eliminar Usuario</h1>
+        <h3>Seleccionar Usuario</h3>
+        <form class="select_delet" action="deletuser.php" method="post" required>
+          <select class="User" name="User" id="User" style="margin-left: 20px; margin-top: 10px;">
+            <?php
+              foreach ($resultado as $usuario) {
+                $id=$usuario["ID_usuario"];
+                $nombre=$usuario["Nombre"];
+                $apellidos=$usuario["Apellidos"];
+                echo "<option value='$id'>$nombre $apellidos</option>";
+              }
+             ?>
+          </select>
+          <br></br>
+          <input id="deletuser"  type="submit" value="Eliminar"></input>
+        </form>
+      </section>
+      <?php include "./assets/piedepagina.php"; ?>
+  </body>
+</html>
