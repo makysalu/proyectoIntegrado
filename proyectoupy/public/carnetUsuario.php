2e4f144c4682443c4ea849d96f4b5cb23d675469
puesvaleok <borjahervas54@gmail.com>
2019-05-24 20:57:03 +0200

    Subo Modificación carnet de usuario

--- proyectoupy/public/carnetUsuario.php
+++ proyectoupy/public/carnetUsuario.php
@@ -11,13 +11,19 @@ session_start();
      <!-- carnetUsuario - Diego Moreno -->
    </head>
    <body>
-     <?php include "./assets/navegador.php"; ?>
+     <div id="textoImagen">Aquí podras ver tu carnet de afiliado.</div>
+         <div><img id="sectionFlecha" src="./images/flecha.png"/></div>
+<?php include "./assets/navegador.php"; ?>
+<div class="fondoColor"></div>
+<div class="fondoPerfil"><img id="sectionImagen" src="./images/fondoCarnet.jpeg"/></div>
+<div id="textoIndex">
+  <h1 id="tituloPagina">FICHA DE SOCIO</h1>
+</div>
      <section id="fichausuario">
        <header>
-         <h1>FICHA DE SOCIO ULyC</h1>
          <div id="cabecerausuario">
            <div class="cabecerausuario" >
-             <img id="imagen" src="images/logo.jpg" width="120" height="120"/>
+             <img id="imagen" src="images/logo.jpeg" width="120" height="120"/>
            </div>
            <div class="cabecerausuario" id="cabeceraNsocio">
                <span><strong>Fecha: </strong><?php echo $_SESSION["user"]['fecha_alta'] ?></span><br><br>
@@ -25,11 +31,11 @@ session_start();
            </div>
            <div class="cabecerausuario">
              <p>
-                 <span>C/MAESTRO CEBRIAN Nº 4 1º</span><br>
+                 <span>Av. Generalitat Valenciana Nº 4 1º</span><br>
                  <span>23008 CATARROJA</span><br>
-                 <span>TLF: 969 528 436</span><br>
-                 <span>FAX: 969 634 782</span><br>
-                 <span>ulyc@hotmail.com</span>
+                 <span>Tlf: 91 141 96 07</span><br>
+                 <span>Fax: 915 99 19 08</span><br>
+                 <span>upep@hotmail.com</span>
              </p>
            </div>
          </div>
@@ -38,17 +44,17 @@ session_start();
        <article>
        <article id="datosficha">
          <header>
-           <span>@ULyC</span>
+           <span>@UpeP</span>
          </header>
          <div id="carnetderecha">
            <div id="fotocarnet">
-             <img src="images/fotocard.jpg" width="240" height="300"/>
+             <img src="images/user.jpg" width="240" height="300"/>
            </div>
          </div>
          <div id="carnetizquierda">
            <div id="logocarnet">
-             <img  src="images/logo.jpg" width="70" height="70"/>
-             <span>ULyC</span>
+             <img  src="images/logo.jpeg" width="70" height="70"/>
+             <span>UpeP</span>
            </div>
            <div id="datoscarnet">
              <span><strong>Carnet de Afiliado</strong></span><br>
